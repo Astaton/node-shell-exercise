323e4765cadc998a271640a79d0bb2b157426a7d
AAron Staton <Orson108@hotmail.com>
2020-01-21 14:03:42 -0500

    ran git init, installed request package, added curl functionality

--- bash.js
+++ bash.js
@@ -1,23 +1,29 @@
 const pwd = require("./pwd");
-const ls = require('./ls');
-const cat = require('./cat')
+const ls = require("./ls");
+const cat = require("./cat");
+const curl = require("./curl");
 
 process.stdout.write("prompt >");
 
 process.stdin.on("data", data => {
-  const cmd = data.toString().trim().split(' ');
+  const cmd = data
+    .toString()
+    .trim()
+    .split(" ");
 
   if (cmd[0] === "pwd") {
     pwd(__dirname);
-  } else if (cmd[0] === 'ls') {
+  } else if (cmd[0] === "ls") {
     ls();
-  } else if (cmd[0] === 'cat')  {
-    cat(cmd[1])
-  }  else {
+  } else if (cmd[0] === "cat") {
+    cat(cmd[1]);
+  } else if (cmd[0] === "curl") {
+    curl(cmd[1]);
+  } else {
     process.stdout.write("You typed: " + cmd);
   }
 
   setTimeout(() => {
-    process.stdout.write("\nprompt > ")
+    process.stdout.write("\nprompt > ");
   }, 5);
 });
