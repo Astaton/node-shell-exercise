ea4f47b49a2347d50f83486e2bc5b69580666ee3
Leslie <lesliew1993@gmail.com>
2020-01-21 13:03:04 -0500

    added cat functionality

--- bash.js
+++ bash.js
@@ -1,15 +1,23 @@
 const pwd = require("./pwd");
+const ls = require('./ls');
+const cat = require('./cat')
 
 process.stdout.write("prompt >");
 
 process.stdin.on("data", data => {
-  const cmd = data.toString().trim();
+  const cmd = data.toString().trim().split(' ');
 
-  if (cmd === "pwd") {
+  if (cmd[0] === "pwd") {
     pwd(__dirname);
-  } else {
+  } else if (cmd[0] === 'ls') {
+    ls();
+  } else if (cmd[0] === 'cat')  {
+    cat(cmd[1])
+  }  else {
     process.stdout.write("You typed: " + cmd);
   }
 
-  process.stdout.write("\nprompt > ");
+  setTimeout(() => {
+    process.stdout.write("\nprompt > ")
+  }, 5);
 });
